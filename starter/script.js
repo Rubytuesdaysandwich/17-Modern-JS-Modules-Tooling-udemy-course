1c3f935b158890c792a06dbe9ef6d73edf79b5f9
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-10-21 14:27:50 -0600

    update file moved

--- starter/script.js
+++ starter/script.js
@@ -95,3 +95,16 @@ export addToCart = function (product, quantity) {
 
 //importing an API introduction to NPM
 import { cloneDeep } from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
